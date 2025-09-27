7b004c5351d1a0c298eb1cd32b7c7d5de8780b8d
QuyTruong <truong.4725212@gmail.com>
2025-09-27 22:55:47 +0700

    update

--- Condition.py
+++ Condition.py
@@ -11,6 +11,20 @@ elif a >= 5 and a < 7:
 else:
     print("yeu")
 
+# Ngoài ra ta có thể lồng 1 điều kiện bên trong 1 điều kiện khác hay còn được gọi là "nested condition"
+a = 0
+if a > 0:
+    # Đây có thể gọi là "nested condition" từ khóa này có xuất hiện trong câu 2. của Exercise: Level 1
+    if a % 2 == 0:
+        print('A is a positive and even integer')
+    else:
+        print('A is a positive number')
+elif a == 0:
+    print('A is zero')
+else:
+    print('A is a negative number')
+
+
 # Exercise
 # Kiểm tra độ tuổi lái xe
 userAge = int(input("Enter your age: "))
